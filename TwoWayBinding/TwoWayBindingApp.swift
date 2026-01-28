5b74474222c2359fd240ceef62360b4eeb5294f9
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-28 15:27:04 -0500

    Initial Commit

---
+++ TwoWayBinding/TwoWayBindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoWayBindingApp.swift
+//  TwoWayBinding
+//
+//  Created by Nia Mitchell on 1/28/26.
+//
+
+import SwiftUI
+
+@main
+struct TwoWayBindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
