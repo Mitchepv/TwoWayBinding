aa00652f8d1936e9a8f7ae1d741c44e648a4f37d
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-28 23:41:23 -0500

    Chapter 1: Fundamental binding

--- TwoWayBinding/ContentView.swift
+++ TwoWayBinding/ContentView.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pressCount = 0
+    @State private var toggleIsOn = true
+    @State private var name = ""
+    @State private var selectedColor: Color = .purple
+    @State private var selectedDate = Date()
+    @State private var stepperValue = 1
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Press Count: \(pressCount) ")
+            
+            Button ("press me"){
+                pressCount += 1
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Spacer()
+            
+            TextField("Enter a name", text: $name)
+                .textFieldStyle(.roundedBorder)
+            Text("Name entered: \(name)")
+            
+            Spacer()
+            
+            Toggle( "Toggle is \(toggleIsOn ? "on" : "off")", isOn: $toggleIsOn)
+            
+            Spacer()
+            
+            Rectangle()
+                .fill(selectedColor)
+                .frame(width: 100, height: 100)
+            
+            ColorPicker("pick a color:", selection: $selectedColor)
+            
+            Spacer()
+            
+            DatePicker( "Date", selection: $selectedDate)
+            Text ("Selected Date is: \(selectedDate.formatted(date: .abbreviated, time: .shortened))")
+            
+            Spacer()
+            
+            Stepper( "Stepper Value: \(stepperValue)", value: $stepperValue, in: 1...10)
+            
+            
+            Spacer()
+            
         }
+        .font(.title2)
         .padding()
     }
 }
